6c49b82d1b5646bb9bc60884fad6b217a30a7d9f
IlliaMamonov <iliamamonov2@gmail.com>
2021-12-19 15:54:32 +0200

    Access token changed again

--- DropBox/RequestSender.cs
+++ DropBox/RequestSender.cs
@@ -4,7 +4,7 @@ namespace DropBox
 {
     public abstract class RequestSender
     {
-        protected static readonly string _token = "Bearer sl.A-YJxi9eP506xC0LOtuw2835piDkSaUDurWdO47W2GJdRwZSoBAkO2N3pi6-mMVXCugEybLu8Hxsh0vL5EW9oQzEVDCcyjfnI2xekWCWrUIoNo8y-y0-zyyA8liGvfsHfQqq0p0";
+        protected static readonly string _token = "Bearer -MOdbWNZszcAAAAAAAAAAbtCE3IppawONF1s1pPR-4wPIJvTalAXfQcL4hMAyZ0L";
         protected IRestClient _client;
         protected IRestRequest _request;
         protected IRestResponse _response;
